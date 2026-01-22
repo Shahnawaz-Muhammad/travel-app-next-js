b7218e6ae28e87b4c6ed65461f08673eaf29d7b2
Shahnawaz-Muhammad <shahnawazmuhammad210@gmail.com>
2026-01-22 21:13:46 +0500

    cleanup: remove root frontend config files

--- types/destinations.ts
+++
@@ -1,9 +0,0 @@
-export interface Destination {
-    id: number,
-    name: string,
-    country: string,
-    description: string,
-    packages: number,
-    image: string,
-    featured: boolean
-}
